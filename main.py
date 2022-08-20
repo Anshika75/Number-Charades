1d3ee64cc2c9a80c181c834ec3b264a95c79abb0
Anshika <anshikathakur596@gmail.com>
2022-08-21 03:29:39 +0530

    format function not working

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import random 
 level = 1
 attempt = 0
-level = int(input("""Choose the level of difficulty: 
+choice = 1
+chances = 10
+
+
+
+while choice == 1:
+    
+    level = int(input("""Choose the level of difficulty: 
 1 - Easy
 2 - Intermediate
 3 - Hard
 4 - God
 """))
-choice = 1
-while choice == 1:
     if level == 1:
+        chances = 10
         limit = 50
     elif level <= 4:
+        chances = 15//level
         limit= 10**level
     else:
         print("Please enter correct level")
         exit()
+    print("You have {} chances to guess the number in the range [1, {}]".format(chances).format(limit))
     print(choice)
     print(limit)
     number = random.randint(1, limit)
