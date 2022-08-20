5666d62203fff3aa4c035346186b791bdd7f8d72
Anshika <anshikathakur596@gmail.com>
2022-08-20 23:49:23 +0530

    Level decision

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-
+import random 
+number = random.randint(1, 100)
+print(number)
+level = 1
+attempt = 0
+level = int(input("""Choose the level of difficulty: 
+1 - Easy
+2 - Intermediate
+3 - Hard
+4 - God
+"""))
+if level == 1:
+    limit= [0, 50]
+elif level <= 4:
+    limit= [0, 10**level]
+else:
+    print("Please enter correct level")
+    exit()
+print(limit)
