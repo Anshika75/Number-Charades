b7ce102ca24ab23fa993d049af9b926d2bbcd808
Himanshu kumar <83366556+I-Himanshu@users.noreply.github.com>
2022-08-22 23:17:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import random
-attempt = 5
 choice = 1
 
 while choice == 1:
@@ -19,8 +18,28 @@ while choice == 1:
         print("Please enter correct level")
         exit()
     print("You have {} chances to guess the number in the range [1, {}]".format(chances,limit))
-    number = random.randint(1, limit)
-    print(number)
+    comp = random.randint(1, limit)
+    while chances > 0:
+        guess = int(input("Enter your guess: "));
+        if guess == comp:
+            print("You Won");
+            break;
+        elif guess > comp:
+            if guess - comp < limit//5:
+                print("You Guess SLigltyy High Number");
+            else:
+                print("You Guessed High Number")
+        else:
+            if comp - guess < limit//5:
+                print("You Guess SLigltyy low Number");
+            else:
+                print("You Guessed Low Number");
+        chances -= 1;
+        if chances == 0:
+            print("You idiot, lost the game")
+            break
+        print(f"{chances} chances left")
+    print("Number Is : ",comp)
     choice = int(input("""Enter 0 to exit
 Enter 1 to restart
 """))
